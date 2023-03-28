30c66d42d477e6790cbc89782dc4d7982ed49c6f
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2023-03-28 09:59:53 -0600

    Backend dinamico

--- cotiapp-desktop/src/constants.js
+++ cotiapp-desktop/src/constants.js
@@ -1,6 +1,8 @@
 const backend = ()=>{
-    //return 'https://cotiapp.onrender.com'
-    return 'http://localhost:4000'
+    const url = document.URL.split('/')[2];
+    return url === 'localhost:3000'
+            ? 'http://localhost:4000'
+            : 'https://cotiapp.onrender.com'
 }
 
 module.exports = backend;
